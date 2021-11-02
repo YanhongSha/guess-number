166f381bd80bc71f2713771037e6e0bcece2e5a6
Yanhong Sha <yanhongsha333@gmail.com>
2021-11-02 14:01:02 +0100

    add count function

--- guess.py
+++ guess.py
@@ -1,14 +1,15 @@
 # guess number
 import random
 r = random.randint(1,50)
-
+count = 0
 while True:
+	count = count + 1
 	x = input('please input a number:')
 	x = int(x)
 	if x == r:
-		print('congratulaitons!')
+		print('congratulaitons! you take', count, 'to get the result')
 		break
 	elif x > r:
-		print('it is too big,try again!')
+		print('it is too big,try again! This is', count, 'times')
 	elif x < r:
-		print('it is too small, try again!')
+		print('it is too small, try again! This is', count, 'times')
